eeeafccffb698067b5e883157bc3361da9d38123
2268849815 <2268849815@qq.com>
2020-02-24 20:57:22 +0800

    online

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
    删除代码， 九八dist/ 所有代码放到根目录下，
 5. git push origin gh-pages
    源码在master分支， 上线的代码在 dist/
+
+1. iview 的from 通过组件和配置的理念， 完爆原生html， css，js写法
+组件的魅力 html 标签是平级的
+Button -> button 比较
+2. Form FormItem i-input Button 组件 表单的UI 文档
+3. :model  :rule="" 验证码是配置出来的 
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,98 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <!-- 表单 -->
+    <div class="info-fo">
+      <!-- Form组件有iview提供 ref 相当与dom 里的id
+      mvvm 数据第一的 获取数据
+       -->
+      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate">
+        <FormItem label="手机号" prop="phone">
+          <i-input placeholder="请输入手机号" size="large" v-model="formValidate.phone">
+          </i-input>
+        </FormItem>
+        <FormItem label="验证码" prop="checkNum">
+          <i-input placeholder="请输入验证码" size="large" v-model="formValidate.checkNum">
+            <Button slot="append" @click="getcheckNum">获取验证码</Button>
+          </i-input>
+        </FormItem>
+        <Button type="error" size="large" long @click="handleSubmit('formValidate')">验证手机号</Button>
+      </Form>
+    </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      formValidate: {
+        phone: '13870403967',
+        checkNum: '1234'
+      },
+      ruleValidate: {
+        phone: [
+          {
+             required: true, message: '手机号不能为空', trigger: 'blur'
+          },
+          {
+            type: 'string', pattern: /^1[3|5|7|8|9][0-9]{9}$/,
+            message: '手机号格式出错', trigger: 'blur'
+          }
+        ],
+        checkNum: [
+          { required: true, message: '验证码不能为空', trigger: 'blur'},
+          { type: 'string', min: 4, max: 4, message: '验证码格式错误', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      // 验证表单 
+      // console.log(this.formValidate)
+      //有通过验证码
+      // console.log(this.$refs);
+      this.$refs.formValidate.validate((valid) => {
+        // console.log(valid);
+        if (valid) {
+          this.$Message.success({
+          content: '成功激活手机号',
+          duration: 6,
+          closeable: true
+        })
+        }
+        if (!valid) {
+          this.$Message.error({
+            content: '表单有问题',
+            duration: 6,
+          closeable: true
+          })
+        }
+      })
+    },
+    getcheckNum () {
+      if (this.formValidate.phone.length === 11) {
+        // this -> vue -> use iview -> Message js 声明调用
+        this.$Message.success({
+          content: '验证码为: 1234',
+          duration: 6,
+          closeable: true
+        });
+      } else {
+        this.$Message.error({
+          content: '请输入正确的手机号',
+          duration: 6,
+          closeable: true
+        })
+      }
+    }
+  }
+}
+</script>
 
 <style>
 #app {
